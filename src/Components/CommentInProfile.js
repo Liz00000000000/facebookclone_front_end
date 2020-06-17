b0d247ea8f2ab227619bfbfc7c21d8f5cb50b475
dkersten <dkersten1994@gmail.com>
2020-06-17 19:57:23 -0400

    styling to comments on user page

--- src/Components/CommentInProfile.js
+++ src/Components/CommentInProfile.js
@@ -8,10 +8,12 @@ export class CommentInProfile extends Component {
         const commenter = this.props.users.find(user => user.id === this.props.user_id)
         console.log(this.props)
         return (
-            <div>
+            <div className="comment">
                 <img src={commenter.picture} alt={commenter.first_name} /> 
                 {commenter.first_name} {commenter.last_name}
+                <div className="comment-content">
                 {this.props.content}
+                </div>
             </div>
         )
     }
